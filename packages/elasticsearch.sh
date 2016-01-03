5153610ed29acb8fe8490ea8da8d43043d98e660
story-stream <github@storystream.it>
2016-01-03 14:34:01 +0000

    Added enabling script execution into elasticsearch config

--- packages/elasticsearch.sh
+++ packages/elasticsearch.sh
@@ -31,6 +31,8 @@ wget --continue --output-document "${CACHED_DOWNLOAD}" "${ELASTICSEARCH_DL_URL}"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${ELASTICSEARCH_DIR}"
 
 echo "http.port: ${ELASTICSEARCH_PORT}" >> ${ELASTICSEARCH_DIR}/config/elasticsearch.yml
+echo "script.inline: sandbox" >> ${ELASTICSEARCH_DIR}/config/elasticsearch.yml
+echo "script.engine.groovy.inline.update: on" >> ${ELASTICSEARCH_DIR}/config/elasticsearch.yml
 
 # Make sure to use the exact parameters you want for ElasticSearch and give it enough sleep time to properly start up
 nohup bash -c "${ELASTICSEARCH_DIR}/bin/elasticsearch 2>&1" &
